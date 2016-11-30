982f65ff764501777cc2c19d304bd5d2c921d07c
Vuyisile Ndlovu <terrameijar@gmail.com>
2016-11-30 17:54:27 +0200

    Inputbox added to form

--- functional_tests.py
+++ functional_tests.py
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
         #She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.getattribute('placeholder'),
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
         # She types "Buy peacock feathers" into a text box(Edith's hobby is tying
@@ -35,20 +35,21 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', 
+            [row.text for row in rows]
         )
-
         #There is still a text box inviting her to add another item. She
-        # enters "User peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        
         # The page updates again, and now shows both items on her list
         # Edit wonder's whether the site will remember her list. Then she sees
         # that the site has generated a unique url for her -- ther is some 
         # explanatory text to that effect.
+        self.fail('Finish the test!')
         # She visits that url -her todo list is still there.
         # satisfied, she goes back to sleep
 
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+        })
