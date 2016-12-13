57a9b08ab58c168603da9bda5755be9d2e95a20e
Vuyisile Ndlovu <terrameijar@gmail.com>
2016-12-13 04:13:22 +0200

    New URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,23 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.models import Item 
 
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        #We're using the Django test client instead of calling view directly
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -58,19 +75,9 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(),0)
-
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
--- lists/views.py
+++ lists/views.py
@@ -7,9 +7,12 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def home_page(request):
     if request.method == 'POST':
-        #new_item_text = request.POST['item_text']
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')   
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})   
